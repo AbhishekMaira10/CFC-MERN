3957f815a1377c874659a9da60438c338d50dc9c
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2021-04-05 19:07:09 +0530

    add codes

--- Lecture 3/function.js
+++ Lecture 3/function.js
@@ -1,6 +1,22 @@
-function happy(){
-  console.log("I AM HAPPy");
+function happy() {
+  console.log('I AM HAPPy')
 }
 
 setInterval(happy, 1000) // runs for 1 second
 setTimeout(happy, 1000) // runs once for 1 second
+
+let arr = [10, 21, 20, 0, '']
+
+let out = arr.filter(function (item) {
+  return item
+})
+
+let arr2 = []
+
+function hey(name) {
+  console.log('Hurray ' + name)
+}
+
+arr2.push(hey)
+
+arr2[0]('Harry')
